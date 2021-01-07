64af9cc237770514df3ad8df2e84ffd59b471d72
alichbouki <75620983+alichbouki@users.noreply.github.com>
2021-01-07 15:05:51 +0100

    insertion de LED on/off, le bizzo et le boutton, et l'activation de ses dergner

--- arduino_coke_time/arduino_coke_time.ino
+++ arduino_coke_time/arduino_coke_time.ino
@@ -1,14 +1,27 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-  
-LiquidCrystal_I2C lcd (0x27,16,2);
+
+LiquidCrystal_I2C lcd (0x27, 16, 2);
+
+int led_Off = 2;
+int led_On = 3;
+int buz = 4;
+int btn = 5;
 
 void setup() {
-  lcd.begin(16,2);
+
+  lcd.begin(16, 2);
   lcd.init();
   lcd.setBacklight(HIGH);
+
+  pinMode(btn, INPUT_PULLUP);
+
+  for (int i = led_On; i < 6; i++) {
+    pinMode(i, OUTPUT);
+  }
+
 }
 
 void loop() {
-  
+
 }
